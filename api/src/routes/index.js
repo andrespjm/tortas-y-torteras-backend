1e699aa73f745711ab887fcf8d0d7ec2348f3b41
MEugeV <eugeniavogt@yahoo.com.ar>
2022-08-16 22:38:24 -0300

    feat(src/controllers/colors.controller.js): get colors, fix json

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import products from './products.router.js';
+import colors from './colors.router.js';
 const router = Router();
 
 router.use('/products', products);
+router.use('/colors', colors);
 
 export default router;
