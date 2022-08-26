c679a118034b2865ca05a6b3d2d9f3a8fd6a1c0d
unknown <omarramirez5876@gmail.com>
2022-08-26 16:10:04 -0500

    deleting nodemailer from routes

--- api/src/routes/purchases.router.js
+++ api/src/routes/purchases.router.js
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 import { Router } from 'express';
-import ejs from 'ejs';
 import {
 	deletePurchase,
 	getPurchase,
@@ -11,7 +10,7 @@ import {
 	updatePurchaseCart,
 	updatePurchaseAdmin,
 } from '../controllers/purchases.controller.js';
-import { sendMail } from '../nodemailer/sendMail.js';
+
 const router = Router();
 
 router.get('/', async (req, res) => {
@@ -69,13 +68,7 @@ router.post('/:userId', async (req, res) => {
 		const { userId } = req.params; 
 		const purchaseData = req.body;
 		const purchase = await postPurchases(purchaseData, userId);
-		const purchaseMail = await getPurchases(userId); 	//	get info to send a mail
-		ejs.renderFile('../views/successfulPurchase.ejs', {purchaseMail}, (err, data) => {
-			if(err) console.log(err)
-			else{
-				sendMail(purchaseMail[0].User.email, 'Successful Purchase', data)
-			}
-		})
+
 		res.status(200).send(purchase);
 	} catch (error) {
 		res.status(500).send(error.message);
@@ -124,13 +117,6 @@ router.put('/user/:id', async (req, res) => {
 					shippingAddressNumber
 				)
 			);
-			const purchaseMail = await getPurchases(id);
-			ejs.renderFile('../views/purchaseStatus.ejs', {purchaseMail}, (err, data) => {
-				if(err) console.log(err)
-				else{
-					sendMail(purchaseMail[0].User.email, 'Successful Purchase', data)
-				}
-			})
 
 	} catch (error) {
 		res.status(500).send(error.message);
