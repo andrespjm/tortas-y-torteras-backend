89a8181911e06597d8f0dd2195d4eb3c0c6e838b
MEugeV <eugeniavogt@yahoo.com.ar>
2022-09-16 20:23:24 -0300

    sync

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import app from './app.js';
-import { setJsonColors } from './controllers/colors.controller.js';
-import {
-	setJsonProducts,
-	setJsonProductTypes,
-} from './controllers/products.controller.js';
-import { setJsonPurchases } from './controllers/purchases.controller.js';
-import { setJsonUsers } from './controllers/users.controller.js';
-import { setJsonReviews } from './controllers/reviews.controller.js';
+// import { setJsonColors } from './controllers/colors.controller.js';
+// import {
+// 	setJsonProducts,
+// 	setJsonProductTypes,
+// } from './controllers/products.controller.js';
+// import { setJsonPurchases } from './controllers/purchases.controller.js';
+// import { setJsonUsers } from './controllers/users.controller.js';
+// import { setJsonReviews } from './controllers/reviews.controller.js';
 import { sequelize } from './db/database.js';
 import { verifyDir } from './helpers/verify-dir.js';
 const { PORT } = process.env;
@@ -17,12 +17,12 @@ const PORT_SERVER = PORT || 3001;
 	try {
 		await sequelize.sync({ force: true });
 		console.log('Connection has been established successfully');
-		await setJsonColors();
-		await setJsonUsers();
-		await setJsonProductTypes();
-		await setJsonProducts();
-		await setJsonPurchases();
-		await setJsonReviews();
+		// await setJsonColors();
+		// await setJsonUsers();
+		// await setJsonProductTypes();
+		// await setJsonProducts();
+		// await setJsonPurchases();
+		// await setJsonReviews();
 		verifyDir();
 		app.listen(PORT_SERVER, () =>
 			console.log('Server is running on port: ', PORT_SERVER)
